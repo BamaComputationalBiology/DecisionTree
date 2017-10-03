ec3e47fe0e28f3cd397aed333133d08e4737c099
jannafierst <jlfierst@ua.edu>
2017-10-03 16:36:19 -0500

    Update split_fasta.sh

--- decision_example/split_fasta.sh
+++ decision_example/split_fasta.sh
@@ -4,9 +4,9 @@
 
 #!/bin/bash
 
-awk '/^>scaffold/ {OUT=substr($0,2) ".fasta"}; OUT {print >OUT}' final.assembly.fasta
-cat scaffolds_to_keep.txt | while read line; do cat "scaffold_"$line".fasta" ; done > kept_scaffolds.fasta
-rm "scaffold"*".fasta"
+awk '/^>scaffold/ {OUT=substr($0,2) ".fasta"}; OUT {print >OUT}' final.assembly.fasta # split .fasta by scaffold and print to new files
+cat scaffolds_to_keep.txt | while read line; do cat "scaffold_"$line".fasta" ; done > kept_scaffolds.fasta # read white list of scaffolds and create new genome .fasta
+rm "scaffold"*".fasta" # remove intermediate files
 
 
 # bash line for checking consistency between the whitelist and the new scaffolds file
